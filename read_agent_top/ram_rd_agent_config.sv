8c0e6aef469fd37f9aeb8485286db6ca66e3da30
MaheshbabuAmuda <87308339+MaheshbabuAmuda@users.noreply.github.com>
2026-09-08 20:51:22 +0530

    ram_rd_agent_config

---
+++ read_agent_top/ram_rd_agent_config.sv
@@ -0,0 +1,26 @@
+class ram_rd_agent_config extends uvm_object;
+
+  // Factory registration
+  `uvm_object_utils(ram_rd_agent_config)
+
+  // Virtual interface
+  virtual ram_if vif;
+
+  // Agent activity mode
+  uvm_active_passive_enum is_active = UVM_ACTIVE;
+
+  // Transaction counters
+  static int mon_rcvd_xtn_cnt = 0;
+  static int drv_data_sent_cnt = 0;
+
+  // Constructor
+  extern function new(string name = "ram_rd_agent_config");
+
+endclass: ram_rd_agent_config
+
+
+// Constructor
+function ram_rd_agent_config::new(string name = "ram_rd_agent_config");
+  super.new(name);
+endfunction: new
+
